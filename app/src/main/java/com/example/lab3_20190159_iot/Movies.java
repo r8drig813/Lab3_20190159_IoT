467470463cdc2d9ee147e72132f3385578318b64
Rodrigo Tinoco <a20190159@pucp.edu.pe>
2024-04-19 00:18:12 -0500

    Casi todas las vistas

---
+++ app/src/main/java/com/example/lab3_20190159_iot/Movies.java
@@ -0,0 +1,19 @@
+package com.example.lab3_20190159_iot;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Movies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies);
+
+    }
+}
